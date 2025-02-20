f7e9ae09a6e72cf667dd6a2c629b9a783a2e7a5d
freddyor <168146737+freddyor@users.noreply.github.com>
2025-02-20 18:53:01 +0000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -3,7 +3,7 @@ export const buildings = [
     name: "York Minster",
     coords: [-1.0834619, 53.9622239],
     image: "images/yorkminster.png",
-    description: "v",
+    description: "",
     occupation: "1220",
     tldr: "A stunning Gothic cathedral, it is the largest of its kind in Northern Europe.",
     events: [
