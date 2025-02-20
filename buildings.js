d112df483ecd985f9217d3a046f954a01918c9fa
freddyor <168146737+freddyor@users.noreply.github.com>
2025-02-20 18:48:10 +0000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -3,12 +3,12 @@ export const buildings = [
     name: "York Minster",
     coords: [-1.0834619, 53.9622239],
     image: "images/yorkminster.png",
-    description: "",
+    description: "v",
     occupation: "1220",
     tldr: "A stunning Gothic cathedral, it is the largest of its kind in Northern Europe.",
     events: [
       { date: "WW2", description: "During World War II, 109 stained glass windows were removed and hidden in secret locations to protect them from bombing." },
-      { date: "2022", description: "King Charles III visited York Minster to unveil a statue of Queen Elizabeth II." }
+      { date: "2022", description: "King Charles III visited York Minster to unveil a statue of Queen Elizabeth II." },
     ]
   },
   // Add more buildings as needed
