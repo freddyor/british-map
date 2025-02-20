2847d6ec29430bf29530837e3ba65f9ae34bfea5
freddyor <168146737+freddyor@users.noreply.github.com>
2025-02-20 19:54:14 +0000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -29,7 +29,7 @@ export const buildings = [
     image: "images/cliffordstower.png",
     description: "",
     occupation: "1086",
-    tldr: "An iconic landmark and the remains of York Castle, offering panoramic views of the city.",
+    tldr: "Originally built by William 1 to dominate the former Viking city.",
     events: [
       { date: "1190", description: "Site of a tragic massacre where about 150 Jews sought refuge but were besieged by a mob." },
       { date: "1665", description: "George Fox, the founder of the Quaker movement, was imprisoned here for two nights." }
@@ -49,23 +49,23 @@ export const buildings = [
   },
 {
     name: "York Guildhall",
-    coords: [,-1.0847032, 53.9600261], // Approximate coordinates
+    coords: [-1.0847032, 53.9600261], // Approximate coordinates
     image: "images/yorkguildhall.png", // Replace with actual image path
     description: "",
     occupation: "1459",
-    tldr: "A historic civic building with a rich political and social history.",
+    tldr: "A historic civic originally built as a meeting place for the City's guilds (merchants).",
     events: [
+      { date: "1483", description: "King Richard III was entertained here." }
       { date: "1647", description: "During the English Civil War, a ransom of £200,000 was counted within its walls to secure the release of Charles I from the Scots." },
-      { date: "1850", description: "Prince Albert, the Prince Consort to Queen Victoria, attended a royal banquet here." }
     ]
   },
  {
     name: "King's Manor",
-    coords: [ -1.0861623, 53.9623288], // Replace with accurate coordinates
+    coords: [-1.0861623, 53.9623288], // Replace with accurate coordinates
     image: "images/kingsmanor.png", // Replace with actual image path
     description: "",
-    occupation: "Medieval", // Replace with accurate date
-    tldr: "A historic building that served as the headquarters of the Council of the North.",
+    occupation: "11th Century", // Replace with accurate date
+    tldr: "A historic building, originally built to house the abbots of St Mary's Abbey.",
     events: [
       { date: "1688", description: "During the Glorious Revolution, the building was dramatically seized from Sir John Reresby, who remained loyal to James II, by forces loyal to William of Orange." },
       { date: "Henry VIII", description: "King Henry VIII stayed at King's Manor when it served as the headquarters of the Council of the North." }
