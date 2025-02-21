df446e767f9c6e09950cb1e70928a92e722b4432
freddyor <168146737+freddyor@users.noreply.github.com>
2025-02-21 01:11:38 +0000

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -50,7 +50,7 @@ export const buildings = [
 {
     name: "Bootham Park Hospital",
     coords: [-1.0854450823370998, 53.9675999686222],
-    image: null,
+    image: "images/bootham.png",
     description: null,
     occupation: "1777",
     tldr: "Originally known as 'County Lunatic Asylum, York'. Criticism about the handling of inmates at the asylum and the death of Hannah Mills, who was a Quaker, led the local Quaker community to found a new asylum known as The Retreat in 1790, effectively replacing this asylum.",
@@ -80,6 +80,128 @@ export const buildings = [
     events: [
       { date: "1688", description: "During the Glorious Revolution, the building was dramatically seized from Sir John Reresby, who remained loyal to James II, by forces loyal to William of Orange." },
       { date: "Henry VIII", description: "King Henry VIII stayed at King's Manor when it served as the headquarters of the Council of the North." }
+    ]
+  },
+ {
+    name: "Castlegate House",
+    coords: [-1.0862176206651686, 53.96250673808531],
+    image: "images/castlegatehouse.png",
+    description: null,
+    occupation: "1763",
+    tldr: "The house is currently used as a Masonic temple.",
+    events: [
+      { date: "1831", description: "The house became the Quaker York Quarterly Meeting Girls' School. Its lease expired in 1857 and the school instead moved to Dalton Terrace, becoming well-known The Mount School." }
+    ]
+  },
+  {
+    name: "Bishopbarns",
+    coords: [-1.1030560599151018, 53.94874114129173],
+    image: "images/bishopbarns.png",
+    description: null,
+    occupation: "1905",
+    tldr: "Walter Brierley, architect of many of York's greatest buildings, built this house for himself.",
+    events: [
+      { date: "2016", description: "It was the first house in York to sell for over £2million." }
+    ]
+  },
+  {
+    name: "Cumberland House",
+    coords: [-1.0828489169782274, 53.956814504153286],
+    image: "images/cumberlandhouse.png",
+    description: null,
+    occupation: "1710",
+    tldr: "It was built by Lord Mayor William Cornwell.",
+    events: [
+      { date: "1746", description: "The house acquired its name from Prince William, Duke of Cumberland, who stayed here following his victory at the Battle of Culloden." }
+    ]
+  },
+  {
+    name: "Red Tower",
+    coords: [-1.0713779227295128, 53.95795987409401],
+    image: "images/redtower.png",
+    description: null,
+    occupation: "1490",
+    tldr: "It is the only brick tower in the city.",
+    events: [
+      { date: "BRICK", description: "The Tilers' Guild were employed to build the tower in brick, as it was cheaper than stone. This led to a rift between the Tilers' and the Masons' Guilds with the former asking the city for protection following threats and damage to their tools by the Mason's." },
+      { date: "2018", description: "It was converted into a hub and cafe, for which it won a York Design Award." }
+    ]
+  },
+  {
+    name: "Grays Court",
+    coords: [-1.0803656155532937, 53.96293921598464],
+    image: "images/grayscourt.png",
+    description: null,
+    occupation: "1080",
+    tldr: "Now a hotel, it is one of the oldest continuously occupied dwellings in the UK.",
+    events: [
+      { date: "1547", description: "Edward Seymour, Duke of Somerset, was given the house by Edward VI, the son of Henry VIII." },
+      { date: "1788", description: "The house belonged to the Gray family. There have been two Lord Mayors from the Gray family and the solicitors outside the Minster was created by William Gray in 1695." }
+    ]
+  },
+  {
+    name: "Goddards",
+    coords: [-1.1039493013859114, 53.940387597936834],
+    image: "images/goddards.png",
+    description: null,
+    occupation: "1927",
+    tldr: "This building was built for Noel Terry MBE, from the famous Terry family (Terry's chocolate).",
+    events: [
+      { date: "1930s", description: "Noel lived in this house when he introduced the famous Chocolate Orange to the world." }
+    ]
+  },
+  {
+    name: "Garforth House",
+    coords: [-1.0872525932033406, 53.95744218582411],
+    image: "images/garforthhouse.png",
+    description: null,
+    occupation: "1757",
+    tldr: "The Georgian townhouse was St Margaret's Independent Grammar School for Girls, run by nuns from 1918-1968. Each year, the current owners host the nuns, now in their 90s, for a school reunion.",
+    events: []
+  },
+  {
+    name: "The Grand",
+    coords: [-1.0889314522636129, 53.9585617368568],
+    image: "images/thegrand.png",
+    description: null,
+    occupation: "1906",
+    tldr: "The Grand was originally the headquarters of North Eastern Railway, then one of the richest businesses in Britain.",
+    events: [
+      { date: "1904", description: "The design won a silver medal at an exhibition in Paris." },
+      { date: "2010", description: "The hotel opened, which now has 207 rooms." }
+    ]
+  },
+  {
+    name: "Jacobs Well",
+    coords: [-1.0884877806115956, 53.9568223082359],
+    image: "images/jacobswell.png",
+    description: null,
+    occupation: "1474",
+    tldr: "The building is named after Jacob's Well in Palestine, where Jesus is said to have spoken to a Samaritan woman.",
+    events: []
+  },
+  {
+    name: "Micklegate House",
+    coords: [-1.0891909200470318, 53.95703137954276],
+    image: "images/micklegatehouse.png",
+    description: null,
+    occupation: "1752",
+    tldr: "Now a hotel, it was built to be a town house for John Bourchier, owner of Beningborough House",
+    events: [
+      { date: "1960", description: "It housed the Depart of Mathematics for York University, then the Department of Archaeology 1978-1996." }
+    ]
+  },
+  {
+    name: "The Bar Convent",
+    coords: [-1.0913957008761188, 53.9552023778525],
+    image: "images/barcovenant.png",
+    description: null,
+    occupation: "1686",
+    tldr: "It is the oldest surviving Catholic covenant on the British isles.",
+    events: [
+      { date: "1696", description: "The building was attacked by an angry mob. St Michael is engraved on the front door as it is believed he appeared over the house and scared them off." },
+      { date: "NAPOLEON", description: "The Covenant provided shelter for fleeing continental priests during the Napoleonic Wars." },
+      { date: "WW2", description: "5 nuns were killed as the building was bombed." }
     ]
   },
 
