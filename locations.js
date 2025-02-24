c578101cd7f02e0cccdc1912ffbd5b6f644c7a98
freddyor <168146737+freddyor@users.noreply.github.com>
2025-02-24 10:43:15 +0000

    Update locations.js

--- locations.js
+++ locations.js
@@ -596,7 +596,7 @@ export const locations = [
     ]
   },
 { 
-    coords: [-1.0906194, 53.9647176],
+    coords: [ -1.0818266299937829, 53.961879768431956],
     name: "Constantine",
     occupation: "272-337",
     description: "Constantine was proclaimed Roman Emporer on this spot.",
