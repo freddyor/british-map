9a80cedacfa7b196969bcf674f5c4ef9aeeb01a8
freddyor <168146737+freddyor@users.noreply.github.com>
2025-03-10 03:48:27 +0000

    Update imageAttributions.js

--- imageAttributions.js
+++ imageAttributions.js
@@ -2,6 +2,41 @@
 
 // Array to store image attributions
 const imageAttributions = [
+          {
+      "name": "<a href='https://commons.wikimedia.org/wiki/User:Royal_Society_uploader'>James Dyson</a>",
+      "author": "The Royal Society",
+      "license": "CC BY-SA 3.0"
+    },
+          {
+      "name": "<a href='https://www.flickr.com/photos/kingkongphoto/5113074392/'>Princess Diana</a>",
+      "author": "John Matthew Smith & www.celebrity-photos.com",
+      "license": "CC BY-SA 2.0"
+    },
+          {
+      "name": "<a href='https://digital.library.ucla.edu/catalog/ark:/21198/zz0002pv3r'>John Lennon</a>",
+      "author": "Tony Barnard, Los Angeles Times",
+      "license": "CC BY-SA 4.0"
+    },
+          {
+      "name": "<a href='https://www.nli.org.il/he/images/NNL_ARCHIVE_AL990040462240205171/NLI'>Margaret Thatcher</a>",
+      "author": "Israel Press and Photo Agency",
+      "license": "CC BY-SA 4.0"
+    },
+          {
+      "name": "<a href='https://www.flickr.com/photos/evarinaldiphotography/6983823195/'>Michael Crawford</a>",
+      "author": "Eva Rinaldi",
+      "license": "CC BY-SA 2.0"
+    },
+          {
+      "name": "<a href='https://www.flickr.com/photos/27077452@N04/4513125422'>J.K. Rowling</a>",
+      "author": "Daniel Ogren",
+      "license": "CC BY-SA 2.0"
+    },
+               {
+      "name": "<a href='https://commons.wikimedia.org/wiki/File:Weston_Library_Opening_by_John_Cairns_20.3.15-139_David_Attenborough.jpg'>David Attenborough</a>",
+      "author": "John Cairns",
+      "license": "CC BY-SA 4.0"
+    },
      {
       "name": "<a href='https://www.flickr.com/photos/websummit/54135747450/'>Tim Berners Lee</a>",
       "author": "Web Summit",
