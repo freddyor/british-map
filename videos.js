79138e29c75cf9157a1b9f96fcff0f06a2408395
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-08 00:46:22 +0100

    Create videos.js

---
+++ videos.js
@@ -0,0 +1,9 @@
+export const videos = [
+    {
+        name: "Sample Video",
+        coords: [-1.1362937813011333, 54.08187448382417],
+        videoUrl: "images/huby.mp4",
+        description: "This is a sample video description."
+    },
+    // Add more video marker data here
+];
