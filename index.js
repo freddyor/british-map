b3e0311d5982989f252ff2246132fd424e8dfa47
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-08 05:32:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -295,7 +295,7 @@ function createPopupContent(location, isFirebase = false) {
         <!-- Conditional rendering of the video element -->
         ${videoUrl ? `
             <div style="margin-top: 10px; width: 100%;">
-                <iframe width="100%" height="calc(100% * 16 / 9)" src="${videoUrl}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="display: block; width: 100%;"></iframe>
+                <iframe width="100%" height="calc(100vw * 16 / 9)" src="${videoUrl}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="display: block; width: 100%; height: calc(100vw * 16 / 9); margin: 0; padding: 0;"></iframe>
             </div>
         ` : ''}
     `;
