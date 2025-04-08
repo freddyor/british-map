a94048eb96a9a3500b884ba5af84245e943f60b2
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-08 02:14:33 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -6,11 +6,6 @@ export const videoMarkers = [
         videoUrl: "https://example.com/york-tour.mp4",
         thumbnail: "images/gateforthhall.png" // Optional
     },
-    {
-        coordinates: [-2.9916, 53.4084], // Liverpool coordinates
-        title: "Liverpool Docks",
-        videoUrl: "https://example.com/liverpool.mp4"
-        // thumbnail omitted = will use default marker
-    }
+
 ];
 
