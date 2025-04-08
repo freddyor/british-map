0a26dd108f0b9b99eccd6855bd57765e2259feb7
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-08 01:31:10 +0100

    Update videos.js

--- videos.js
+++ videos.js
@@ -2,7 +2,7 @@ export const videos = [
     {
         name: "Hello Video",
         coords: [-1.0835104081554843, 53.95838745239521],
-        videoUrl: "videos/IMG_4629.mov",
+      videoUrl: "https://github.com/freddyor/british-map/raw/main/videos/IMG_4629.mov",
         description: "This is a hello video."
     },
     // Add more video marker data here if needed
