5ac0f9cf69a4e236ba39339f4af78dbf4b6f2025
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-11 17:32:06 +0100

    Update imageAttributions.js

--- imageAttributions.js
+++ imageAttributions.js
@@ -2,6 +2,414 @@
 
 // Array to store image attributions
 const imageAttributions = [
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:York_Minster_from_M%26S.JPG'>York Minster</a>",
+    "author": "MatzeTrier",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:York_Castle_Museum.jpg'>York Castle Museum</a>",
+    "author": "Jitka Erbenová",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:B%C3%A2timent_Grays_Solicitors_York_1.jpg'>York Dispensary</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/atoach/5259021070/'>Clifford's Tower</a>",
+    "author": "Tim Green",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/1155772'>Fairfax House</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/15181848@N02/51980817502/'>The Great East Window</a>",
+    "author": "amandabhslater",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/92044'>Bootham Park Hospital</a>",
+    "author": "Gordon Kneale Brooke",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/7493097'>York Guildhall</a>",
+    "author": "Stephen Craven",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/atoach/26233677684/'>King's Manor</a>",
+    "author": "Tim Green",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/857017'>Castlegate House</a>",
+    "author": "Richard Croft",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/2993569'>Bishopsbarns</a>",
+    "author": "Gareth James",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/atoach/4198379598/'>Cumberland House</a>",
+    "author": "Tim Green",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:The_Red_Tower_on_York_city_walls_(5).JPG'>The Red Tower</a>",
+    "author": "Harry Mitchell",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Grays_Court_York.jpg'>Grays Court</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/3144429'>Goddards</a>",
+    "author": "Bob Cantwell",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/673504'>Garforth House</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:The_Grand_Hotel_%26_Spa,_York.jpg'>The Grand </a>",
+    "author": "Seth Whales",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/dun_deagh/15072944989/'>Jacob's Well</a>",
+    "author": "dun_deagh",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Judges_Court.jpg'>Judges Court</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/atoach/4200761860/'>Micklegate House</a>",
+    "author": "Tim Green",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://web.archive.org/web/20161012104109/http://www.panoramio.com/photo/12388879'>Heslington Hall</a>",
+    "author": "simoneseverini",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/1091475'>Bar Convent</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/1691691'>Church of All Saints, York</a>",
+    "author": "Michael Jagger",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:St_Denys_Church_York.JPG'>St Denys Church, York</a>",
+    "author": "GuillaumeTell",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/676090'>Lady Row</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Tour_Lendal_York_2.jpg'>Lendal Tower</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Merchant_Taylors%27_Hall_York.jpg'>Merchant Taylors' Hall</a>",
+    "author": "Kaly99",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Ask_at_the_Assembly_Rooms,_Blake_Street,_York_(21st_October_2010).jpg'>Assembly Rooms</a>",
+    "author": "Mtaylor848",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:NormanHouseYork.jpg'>Norman House</a>",
+    "author": "BZ4brbr",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:St_William%27s_College_York_2023.jpg'>St William's College</a>",
+    "author": "Seasider53",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Hall_Wealden_York_5.jpg'>The Wealden Hall</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Old_St_Lawrence%27s_Church,_York.jpg'>St Lawrence Tower</a>",
+    "author": "GuillaumeTell",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Roman_Fortifications_in_Museum_Gardens_York.jpg'>Multangular Tower</a>",
+    "author": "Kaly99",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Cour_Magistrats_York_6.jpg'>York Magistrates Court</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Treasurer%E2%80%99s_House_2023.jpg'>Treasurer’s House</a>",
+    "author": "Seasider53",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://en.wikipedia.org/wiki/File:War_memorial,_York_-_DSC07862.JPG'>Memorial Gardens</a>",
+    "author": "Rept0n1x",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/jpguffogg/16157008236/'>Five Sisters window, York Minster </a>",
+    "author": "Jules & Jenny",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/676048'>York Minster Library</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Merchant_Adventurers%27_Hall.jpg'>Merchant Adventurers' Hall</a>",
+    "author": "Kaly99",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:York_City_Art_Gallery.JPG'>York Art Gallery</a>",
+    "author": "Kaihsu Tai",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Mansion_House_York.jpg'>Mansion House</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+    {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:St_Marys_Abbey_Church_York.jpg'>St Marys Abbey</a>",
+    "author": "Kaly99",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:St._Leonard_Hospital_Undercroft_York.jpg'>St. Leonard's Hospital</a>",
+    "author": "Kaly99",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Yorkshire_Museum.jpg'>Yorkshire Museum</a>",
+    "author": "Kaly99",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://www.flickr.com/photos/andrewdyer/156011512/'>Le Belfrey</a>",
+    "author": "Andrew Dyer",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:York_Cold_War_Bunker.jpg'>Cold War Bunker</a>",
+    "author": "Mike Peel",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/5435307'>Jubbergate</a>",
+    "author": "Richard Vince",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Micklegate_Bar.jpg'>Micklegate Bar</a>",
+    "author": "Dashwortley",
+    "license": "CC BY-SA 3.0"
+  },
+    {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:12_Clifford_Street_York.jpg'>York Institute of Art, Science and Literature</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:St_Olave%27s_House.jpg'>St Olave's House</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:The_Garth_Marygate_York.jpg'>The Garth</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Roots_restaurant.jpg'>Roots</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://www.geograph.org.uk/photo/407864'>The Judges Lodging</a>",
+    "author": "Stanley Howe",
+    "license": "CC BY-SA 2.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:St_Helen%27s_Square,_York_-_DSC07915.JPG'>St Helen's Square</a>",
+    "author": "Rept0n1x",
+    "license": "CC BY-SA 3.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:The_Windmill,_York.jpg'>The Windmill</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+      {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Bay_Horse_Inn.jpg'>Bay Horse Inn</a>",
+    "author": "Jhsteel",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Colliergate_drill_hall_2023.jpg'>Colliergate Drill Hall</a>",
+    "author": "Seasider53",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:%C3%89glise_St_Andrew_York_3.jpg'>St Andrew's Church</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Red_House_Antiques_Centre.jpg'>Red House</a>",
+    "author": "Jhsteel",
+    "license": "CC BY-SA 3.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:War_memorial_in_York_(8400).jpg'>Boer War Memorial</a>",
+    "author": "Nilfanion",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Masonic_Hall,_Duncombe_Place.jpg'>Masonic Hall</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://www.flickr.com/photos/atoach/11313553473/'>De Grey Rooms</a>",
+    "author": "Tim Green",
+    "license": "CC BY-SA 2.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Roman_Bath_York.jpg'>Roman Bath York</a>",
+    "author": "Fingalo Christian Bickel",
+    "license": "CC BY-SA 2.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:York_College_for_Girls.jpg'>York College for Girls</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Maison_River_York_3.jpg#filehistory'>River House</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+        {
+    "name": "<a href='https://www.geograph.org.uk/photo/3751221'>1 Museum Street</a>",
+    "author": "Ian S",
+    "license": "CC BY-SA 2.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Roman_column,_York,_2023.jpg'>Roman column</a>",
+    "author": "Seasider53",
+    "license": "CC BY-SA 4.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:TheMinsterSchoolYork.jpg'>The Minster School</a>",
+    "author": "Rodhullandemu",
+    "license": "CC BY-SA 3.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Old_White_Swan.jpg'>Old White Swan</a>",
+    "author": "Warofdreams",
+    "license": "CC BY-SA 4.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Cosy_Club_Fossgate_York_01.jpg'>Electric Cinema</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:The_Blue_Bell,_York.jpg'>The Blue Bell</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+         {
+    "name": "<a href='https://web.archive.org/web/20161101190946/http://www.panoramio.com/photo/90671700'>Ye Olde Starre Inne</a>",
+    "author": "Paul Lakin",
+    "license": "CC BY-SA 3.0"
+  },
+         {
+    "name": "<a href='https://www.flickr.com/photos/calflier001/9255853199/'>York Railway Station</a>",
+    "author": "calflier001",
+    "license": "CC BY-SA 2.0"
+  },
+         {
+    "name": "<a href='https://www.geograph.org.uk/photo/1110782'>York County Hospital</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+         {
+    "name": "<a href='https://www.geograph.org.uk/photo/5486354'>The Churchill</a>",
+    "author": "Ian Capper",
+    "license": "CC BY-SA 2.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:49_Bootham_York_01.jpg'>49 Bootham</a>",
+    "author": "Malcolmxl5",
+    "license": "CC BY-SA 4.0"
+  },
+         {
+    "name": "<a href='https://www.geograph.org.uk/photo/673252'>Ingram House</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+         {
+    "name": "<a href='https://www.geograph.org.uk/photo/681005'>Wandesford House</a>",
+    "author": "Gordon Hatton",
+    "license": "CC BY-SA 2.0"
+  },
+         {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Maisons_18-22_Coney_Street_York_1.jpg'>Henry Sotheran Ltd</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+           {
+    "name": "<a href='https://commons.wikimedia.org/wiki/File:Maisons_18-22_Coney_Street_York_1.jpg'>20 Coney Street</a>",
+    "author": "Chabe01",
+    "license": "CC BY-SA 4.0"
+  },
+           {
+    "name": "<a href='https://www.yorkpress.co.uk/news/24831133.york-history-story-yorks-real-life-suffragettes/'>Women's Social and Political Union</a>",
+    "author": "David Wilson",
+  },
+           {
+    "name": "<a href='https://www.yorkpress.co.uk/news/24392369.emperors-unexploded-bombs-potted-history-severus-hill/#gallery2'>Severus Hill</a>",
+    "author": " Duncan Marks, York Civic Trust",
+  },
   {
     "name": "<a href='https://www.geograph.org.uk/photo/4083893'>Woodlands Hall</a>",
     "author": "Mike Kirby",
