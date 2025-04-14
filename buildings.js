81ae86bb6aaf32d4eacaf592282ca51838d45e30
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-14 05:57:30 +0100

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -1,10 +1,20 @@
 export const buildings = [
+            {
+    "name": "Great Flood Markers",
+    "coords": [-1.0869728885846095, 53.96042019659332],
+    "image": "https://www.britmap.com/videos/IMG_4866.jpeg",
+    "description": "",
+    "videoUrl": "https://www.britmap.com/videos/copy_1C4F32F7-2469-4054-957C-DFED999308EE.mp4", 
+    "occupation": " ",
+    "tldr": " ",
+    "events": [ ]
+  },
       {
     "name": "Cross Keys Symbols",
     "coords": [-1.0839206184987948, 53.9620920783174],
-    "image": "http://www.historyofyork.org.uk/inc/img.php/tpl/uploads/crossed-keys-york-2.jpg/240/1/fill",
+    "image": "https://live.staticflickr.com/7084/13927402284_f1a3583bb0_b.jpg",
     "description": "",
-    "videoUrl": "https://live.staticflickr.com/7084/13927402284_f1a3583bb0_b.jpg", 
+    "videoUrl": "https://www.britmap.com/videos/copy_80CA9C38-D963-4632-AB3A-DFA00E94E35F.mp4", 
     "occupation": " ",
     "tldr": " ",
     "events": [ ]
