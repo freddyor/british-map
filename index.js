7b88a52d97e5c6de7f0e1149befba51b7bfb4b00
freddyor <168146737+freddyor@users.noreply.github.com>
2025-04-30 04:39:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ addLocationMarkers();
 // Function to dynamically resize markers based on zoom level
 function scaleMarkersBasedOnZoom() {
     const zoomLevel = map.getZoom(); // Get the current zoom level
-    const markerSize = Math.max(0.5, 1.5 * Math.pow(0.8, (20 - zoomLevel))) + 'em'; // Balanced scaling logic
+    const markerSize = Math.max(1, 1.5 * Math.pow(0.2, (20 - zoomLevel))) + 'em'; // Balanced scaling logic
 
     // Update the size of location markers
     document.querySelectorAll('.location-marker').forEach(marker => {
