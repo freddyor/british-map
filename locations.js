da91e0f4f4707f0c94d1628637ce9861f138cfbe
freddyor <168146737+freddyor@users.noreply.github.com>
2025-05-15 00:39:23 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -1,3 +1,11 @@
 export const locations = [
-
+    {
+  name: "Aviva Building",
+  coords: [ -1.0872526854377227, 53.9594037184367 ],
+  image: "https://upload.wikimedia.org/wikipedia/commons/0/04/Aviva%2C_York.jpg",
+  description: "",
+  occupation: "1993",
+  tldr: "This building is home to Aviva insurance company. 🏢 The office has been explicitly designed to refer to the traditional architecture of York; it utilises the same local magnesian limestone as the Minster, while the gabled roofline mirrors the medieval Guildhall across the Ouse. This was not the original plan, but York Civic Trust successfully argued for this more traditional design.",
+  events: []
+}
 ];
