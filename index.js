bd29b0910cb7444dc5ed1cba105647c7c31c92a1
freddyor <168146737+freddyor@users.noreply.github.com>
2025-05-19 05:48:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ mapboxScript.onload = () => {
                 closeBtn.style.border = '1.5px solid #E9E8E0';
                 closeBtn.style.borderRadius = '50%';
                 closeBtn.style.cursor = 'pointer';
-                closeBtn.style.fontSize = '1rem';
+                closeBtn.style.fontSize = '0.7rem';
                 closeBtn.style.zIndex = '100001';
                 closeBtn.style.display = 'flex';
                 closeBtn.style.alignItems = 'center';
