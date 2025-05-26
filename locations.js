697cd2f20949c6f81addeda82e489681ee05ba7e
freddyor <168146737+freddyor@users.noreply.github.com>
2025-05-26 02:09:39 +0100

    Update locations.js

--- locations.js
+++ locations.js
@@ -76,7 +76,7 @@ export const locations = [
      'description': 'Is this the largest architectural downgrade in York’s history?',
      'occupation': '1839',
      'tldr': 'From 1839 to 1963, Salem Chapel sat here on the end of St Saviourgate. The majority of the congregation of Lendal Chapel transferred to this building as it seated 1,700. As at Lendal, there was a schoolroom below. The chapel closed in 1943, and the building was demolished in 1963, replaced by the monstrous Biba House.',
-     'events': []}
+     'events': []
    },
                                     {
   name: "St Sampson’s Square",
