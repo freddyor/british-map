418fba20abf513e3d92502192962ec5962af66e6
freddyor <168146737+freddyor@users.noreply.github.com>
2025-05-26 01:04:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -629,7 +629,7 @@ function createPopupContent(location, isFirebase = false) {
     const eventsData = isFirebase ? data.events : data.events;
     const videoUrl = data.videoUrl ? data.videoUrl : null;
     const tldrContent = !videoUrl
-        ? `<p style="background: #f9f9f9; padding: 10px; margin-top: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); font-size: 15px; color: #7C6E4D;">${data.tldr}</p>`
+        ? `<p style="background: #f9f9f9; padding: 10px; margin-top: 10px; border-radius: 8px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); font-size: 15px; color: #000000;">${data.tldr}</p>`
         : '';
     const imageContent = !videoUrl
         ? `<img src="${data.image || data.imageUrl}" alt="${data.name}" style="width: 80px; height: 80px; object-fit: cover; border-radius: 8px; margin-bottom: 10px;" />`
