113ea53d145d69f0f1249370e427e5cef4ad2951
freddyor <168146737+freddyor@users.noreply.github.com>
2025-06-06 00:10:41 +0100

    Update buildings.js

--- buildings.js
+++ buildings.js
@@ -1,4 +1,13 @@
 export const buildings = [
+  {'name': 'Stuart Feather',
+ 'coords': [-1.0828678455724547, 53.9578982808366],
+ 'image': 'https://www.britmap.com/videos/IMG_6675.jpeg',
+ 'description': '',
+ 'occupation': ' ',
+     "videoUrl": "https://www.britmap.com/videos/Captions_49652B.mp4",
+   "posterUrl": "https://www.britmap.com/videos/IMG_6673.jpeg",
+ 'tldr': ' ',
+ 'events': []},
                                 {
  'name': 'St William',
  'coords': [-1.0814491294017095, 53.9624467151365],
